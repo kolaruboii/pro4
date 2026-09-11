3112a38d74782540cb166258401d25712de9d1f4
ARULMURUGAN-M <arulmurugan2006720@gmail.com>
2026-09-11 11:33:18 +0530

    Update student_solution.sh

--- student_solution.sh
+++ student_solution.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 
 # SELinux Access Denial Practical
-# Student Name:
-# Register Number:
+# Student Name:Arulmurugan M
+# Register Number:1U24IT012
 
 echo "===== SELinux Status ====="
-
+getenforce
+sestatus
 
 echo "===== Creating Web Directory ====="
+sudo mkdir -p /web
 
 echo "===== Creating HTML File ====="
-
+echo "<html><body><h1>SELinux Practical</h1></body></html>" | sudo tee /web/index.html > /dev/null
 
 echo "===== Setting Linux Permissions ====="
-
+sudo chmod 755 /web
+sudo chmod 644 /web/index.html
 
 echo "===== Checking Initial Context ====="
-
+ls -Zd /web
+ls -Z /web/index.html
 
 echo "===== Assigning Wrong SELinux Context ====="
+sudo chcon -t default_t /web/index.html
 
 echo "===== Checking Wrong Context ====="
-
+ls -Z /web/index.html
 
 echo "===== Checking AVC Denials ====="
-echo "===== Correcting SELinux Context ====="
+sudo ausearch -m AVC -ts recent 2>/dev/null
 
+echo "===== Correcting SELinux Context ====="
+sudo restorecon -v /web/index.html
 
 echo "===== Checking Correct Context ====="
-
+ls -Z /web/index.html
 
 echo "===== Practical Completed ====="
